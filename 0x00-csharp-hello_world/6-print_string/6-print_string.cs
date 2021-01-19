d43329b721c7d3d38725781e7333643b8350f657
Giovanni Perez <op7puro@live.com>
2021-01-18 23:41:35 -0500

    Add task 06

---
+++ 0x00-csharp-hello_world/6-print_string/6-print_string.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		string str = "Holberton School";
+        Console.Write(str);
+        Console.Write(str);
+        Console.WriteLine(str);
+        Console.WriteLine("{0}", str.Substring(0, 9));
+		
+    }
+}

8201a40ea72c1b3cfe132840905816a3cbda9f10
Giovanni Perez <op7puro@live.com>
2021-01-20 22:34:18 -0500

    Add task five

---
+++ 0x02-csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class List
+{
+    public static System.Collections.Generic.List<int> CreatePrint(int size)
+    {
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+        string end = "";
+        System.Collections.Generic.List<int> integer = new System.Collections.Generic.List<int>();
+        for (var i = 0; i < size; i++)
+        {
+            integer.Add(i);
+            if (i < size - 1)
+            {
+                end = " ";
+            } else
+            {
+                end = "\n";
+            }
+            Console.Write("{0}{1}", i, end);
+        }
+        return integer;
+    }
+}
+

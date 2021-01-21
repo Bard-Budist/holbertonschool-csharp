3651e3ea2af96fcc71ed5c380c7727ac66ec9b2f
Giovanni Perez <op7puro@live.com>
2021-01-20 23:33:04 -0500

    Add task 10

---
+++ 0x02-csharp-arrays_lists_dictionaries/10-delete_key_value/10-delete_key_value.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Dictionary
+{
+    public static System.Collections.Generic.Dictionary<string, string> DeleteKeyValue(System.Collections.Generic.Dictionary<string, string> myDict, string key)
+    {
+        if (myDict.ContainsKey(key))
+        {
+            myDict.Remove(key);
+        }
+        return myDict;
+    }
+}
+

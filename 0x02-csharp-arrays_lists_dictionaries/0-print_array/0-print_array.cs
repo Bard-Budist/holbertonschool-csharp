ccc90e5f0b5c574d91da831795d9e3a39e36f070
Giovanni Perez <op7puro@live.com>
2021-01-20 20:59:46 -0500

    Add first task

---
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Array
+{
+    public static int[] CreatePrint(int size)
+    {
+        if (size ==  0)
+        {
+            Console.WriteLine("");
+        } else if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+        int[] numbers_array = new int[size];
+        string end = "";
+
+        for (var i = 0; i < size; i++)
+        {
+            numbers_array[i] = i;
+            if (i == size - 1)
+            {
+                end = "\n";
+            } else
+            {
+                end = " ";
+            }
+            Console.Write("{0}{1}", i, end);
+        }
+        return numbers_array;
+    }
+}
+

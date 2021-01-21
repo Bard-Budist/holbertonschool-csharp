cbc677ec256ef25d89ee40d7188d45008df114dc
Giovanni Perez <op7puro@live.com>
2021-01-21 00:19:08 -0500

    Add task 13

---
+++ 0x02-csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Dictionary
+{
+    public static string BestScore(System.Collections.Generic.Dictionary<string, int> myList)
+    {
+        if (myList.Count == 0)
+        {
+            return "None";
+        }
+        string positionMax = "";
+        foreach (var item in myList)
+        {
+            if (positionMax == "")
+            {
+                positionMax = item.Key;
+            }
+            if (item.Value > myList[positionMax])
+            {
+                positionMax = item.Key;
+            }
+        }
+        return positionMax;
+    }
+}
+

575dec4edb6952aca0f8ba5a21874019b2fbdbc5
Giovanni Perez <op7puro@live.com>
2021-01-20 23:10:31 -0500

    Add task 07

---
+++ 0x02-csharp-arrays_lists_dictionaries/7-delete_at/7-delete_at.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class List
+{
+    public static System.Collections.Generic.List<int> DeleteAt(System.Collections.Generic.List<int> myList, int index)
+    {
+        if (myList.Count < index || index < 0)
+        {
+            Console.WriteLine("Index is out of range");
+            return myList;
+        }
+        myList.RemoveAt(index);
+        return myList;
+    }
+}
+

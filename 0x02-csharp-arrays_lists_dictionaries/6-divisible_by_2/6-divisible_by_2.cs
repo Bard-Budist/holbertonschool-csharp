d43abf8a17814a0a00b3640234b7e3a2b4bf05b3
Giovanni Perez <op7puro@live.com>
2021-01-20 22:54:41 -0500

    Add task six

---
+++ 0x02-csharp-arrays_lists_dictionaries/6-divisible_by_2/6-divisible_by_2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class List
+{
+    public static System.Collections.Generic.List<bool> DivisibleBy2(System.Collections.Generic.List<int> myList)
+    {
+        System.Collections.Generic.List<bool> mult_2 = new System.Collections.Generic.List<bool>();
+        for (var i = 0; i < myList.Count; i++)
+        {
+            if (myList[i] % 2 == 0)
+            {
+                mult_2.Add(true);
+            } else
+            {
+                mult_2.Add(false);
+            }
+        }
+        return mult_2;
+    }
+}
+

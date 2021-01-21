3cd4ea096aa61ba01ab547c2fcb5d2f8df03123e
Giovanni Perez <op7puro@live.com>
2021-01-21 11:04:16 -0500

    Add task 15

---
+++ 0x02-csharp-arrays_lists_dictionaries/15-square_matrix/15-square_matrix.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+    class Matrix
+    {
+        public static int[,] Square(int[,] myMatrix)
+        {
+            int[,] squareMatrix = new int[3,3];
+            for (int i = 0; i < myMatrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < myMatrix.GetLength(1); j++)
+                {
+                    int number = myMatrix[i, j];
+                    squareMatrix[i, j] = number * number;
+                }
+            }
+            return squareMatrix;
+        }
+    }
+

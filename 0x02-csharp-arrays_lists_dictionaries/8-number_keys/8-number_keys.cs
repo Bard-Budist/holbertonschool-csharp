41e3070d60920406e122dc9cd28ad0d8a23870d7
Giovanni Perez <op7puro@live.com>
2021-01-20 23:20:33 -0500

    Add task 8

---
+++ 0x02-csharp-arrays_lists_dictionaries/8-number_keys/8-number_keys.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Dictionary
+{
+    public static int NumberOfKeys(System.Collections.Generic.Dictionary<string, string> myDict)
+    {
+        int indexLen = 0;
+        foreach (var item in myDict)
+        {
+            indexLen++;
+        }
+        return indexLen;
+    }
+}

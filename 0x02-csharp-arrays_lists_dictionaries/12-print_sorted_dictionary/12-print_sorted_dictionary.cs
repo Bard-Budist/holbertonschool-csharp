869db9136e0d64459dd2eae6c141583d041dbb37
Giovanni Perez <op7puro@live.com>
2021-01-20 23:56:57 -0500

    Add task 12

---
+++ 0x02-csharp-arrays_lists_dictionaries/12-print_sorted_dictionary/12-print_sorted_dictionary.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Dictionary
+{
+    public static void PrintSorted(System.Collections.Generic.Dictionary<string, string> myDict)
+    {
+        System.Collections.Generic.List<string> sordListKeys = new System.Collections.Generic.List<string>(myDict.Keys);
+        System.Collections.Generic.Dictionary<string, string> dictSorted = new System.Collections.Generic.Dictionary<string, string>();
+        sordListKeys.Sort();
+        for (var i = 0; i < sordListKeys.Count; i++)
+        {
+            dictSorted.Add(sordListKeys[i], myDict[sordListKeys[i]]);
+        }
+        foreach (var item in dictSorted)
+        {
+            Console.WriteLine("{0}: {0}", item.Key, item.Value);
+        }
+    }
+}
+

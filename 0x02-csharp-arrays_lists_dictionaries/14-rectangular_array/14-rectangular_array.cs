9142839a5c5d9b04ad96fa956ca02bc26f5af0fa
Giovanni Perez <op7puro@live.com>
2021-01-21 00:26:56 -0500

    Add task 14

---
+++ 0x02-csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string end = "";
+        for (var i = 0; i < 5; i++)
+        {
+            for (var j = 0; j < 5; j++)
+            {
+                if (j < 4) {
+                    end = " ";
+                } else
+                {
+                    end = "\n";
+                }
+                if (j == 2 && i == 2) {
+                    Console.Write("1{0}", end);
+                } else
+                {
+                    Console.Write("0{0}", end);
+                }
+                
+            }
+            
+        }
+    }
+}
+

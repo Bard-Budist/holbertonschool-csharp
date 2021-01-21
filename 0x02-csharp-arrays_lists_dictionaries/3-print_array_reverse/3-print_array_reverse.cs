d9c297ee8b9f70954b6ca63f87c4e466dbb2c508
Giovanni Perez <op7puro@live.com>
2021-01-20 21:42:07 -0500

    Add 4 task

---
+++ 0x02-csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Array
+{
+    public static void Reverse(int[] array)
+    {
+        string end = "";
+        if (array != null && array.Length > 0)
+        {
+            for (var i = array.Length - 1; i >= 0 ; i--)
+            {
+                if (i - 1>= 0) {
+                    end = " ";
+                } else
+                {
+                    end = "\n";
+                }
+                Console.Write("{0}{1}", array[i], end);
+            }
+        } else {
+            Console.WriteLine("");
+        }
+    }
+}
+

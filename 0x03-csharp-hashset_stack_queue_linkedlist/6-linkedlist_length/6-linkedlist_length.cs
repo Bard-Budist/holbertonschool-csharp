dde2a0043432a9769af87a7180782f38eae0f873
Giovanni Perez <op7puro@live.com>
2021-01-22 09:24:02 -0500

    Add task 06

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/6-linkedlist_length/6-linkedlist_length.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class LList
+{
+    public static int Length(System.Collections.Generic.LinkedList<int> myLList)
+    {
+        int sizeLinked = 0;
+
+        foreach (int item in myLList)
+        {
+            sizeLinked++;
+        }
+        return sizeLinked;
+    }
+}
+

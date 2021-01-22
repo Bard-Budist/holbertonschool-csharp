8f2c820652b4841c209c53c6f1cfa4ed8e84276a
Giovanni Perez <op7puro@live.com>
2021-01-21 23:23:45 -0500

    Add task 03

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -0,0 +1,47 @@
+﻿using System;
+class MyStack
+{
+    public static System.Collections.Generic.Stack<string> Info(System.Collections.Generic.Stack<string> aStack, string newItem, string search)
+    {
+        int sizeStack = aStack.Count;
+        bool containSearch = false;
+
+        Console.WriteLine("Number of items: {0}", sizeStack);
+
+        if (sizeStack == 0)
+        {
+            Console.WriteLine("Stack is empty");
+        } else
+        {
+            Console.WriteLine("Top item: {0}", aStack.Peek());
+        }
+
+        foreach (string item in aStack)
+        {
+            if (item == search)
+            {
+                containSearch = true;
+            }
+        }
+
+        Console.WriteLine("Stack contains \"{0}\": {1}", search, containSearch? "True": "False");
+
+        if (containSearch)
+        {
+            for (var i = 0; i < sizeStack; i++)
+            {
+                if (aStack.Peek() != search)
+                {
+                    aStack.Pop();
+                } else
+                {
+                    aStack.Pop();
+                    break;
+                }
+                
+            }
+        }
+        aStack.Push(newItem);
+        return aStack;
+    }
+}

9ec1a9b2528958e53434cea1eed7f935632ee716
Giovanni Perez <op7puro@live.com>
2021-01-21 20:39:53 -0500

    Add task 02

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class List
+{
+    public static System.Collections.Generic.List<int> CommonElements(System.Collections.Generic.List<int> list1, System.Collections.Generic.List<int> list2)
+    {
+        System.Collections.Generic.List<int> sameIntegers = new System.Collections.Generic.List<int>();
+        foreach (int item in list1)
+        {
+            if (list2.Contains(item))
+            {
+                sameIntegers.Add(item);
+            }
+        }
+        sameIntegers.Sort();
+        return sameIntegers;
+    }
+}
+

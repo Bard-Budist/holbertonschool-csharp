b7b714844e6bec5ee2108f2767fb97672126ecc3
Giovanni Perez <op7puro@live.com>
2021-01-22 09:39:55 -0500

    Add task 07

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/7-linkedlist_add/7-linkedlist_add.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class LList
+{
+    public static System.Collections.Generic.LinkedListNode<int> Add(System.Collections.Generic.LinkedList<int> myLList, int n)
+    {
+        System.Collections.Generic.LinkedListNode<int> node;
+
+        node = myLList.AddFirst(n);
+
+        return node;
+    }
+}
+

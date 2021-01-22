1f0204348b8859e2a9deedee703f7458838b129c
Giovanni Perez <op7puro@live.com>
2021-01-21 23:01:06 -0500

    Add task 02

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class List
+{
+    public static System.Collections.Generic.List<int> DifferentElements(System.Collections.Generic.List<int> list1, System.Collections.Generic.List<int> list2)
+    {
+        System.Collections.Generic.List<int> listDifferents = new System.Collections.Generic.List<int>();
+
+        int position = 0;
+        foreach (int item in list1)
+        {
+            if (!list2.Contains(item))
+            {
+                listDifferents.Add(item);
+            }
+            
+            if (!list1.Contains(list2[position]))
+            {
+                listDifferents.Add(list2[position]);
+            }
+            position++;
+        }
+        listDifferents.Sort();
+        return listDifferents;
+    }
+}
+

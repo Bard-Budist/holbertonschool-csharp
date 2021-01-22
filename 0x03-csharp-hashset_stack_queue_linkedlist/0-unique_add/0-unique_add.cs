33da88db8635e1e7b700d483fe828a0864f23151
Giovanni Perez <op7puro@live.com>
2021-01-21 19:42:52 -0500

    Add task 0

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class List
+{
+    public static int Sum(System.Collections.Generic.List<int> myList)
+    {
+        System.Collections.Generic.List<int> sumUnique =  new System.Collections.Generic.List<int>();
+
+        int sumIntegers = 0;
+        foreach (int item in myList)
+        {
+            if (!sumUnique.Contains(item))
+            {
+                sumIntegers += item;
+                sumUnique.Add(item);
+            }
+        }
+        return sumIntegers;
+
+    }
+}
+

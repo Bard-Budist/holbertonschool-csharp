d0e052a8dfc88005b2133eb95ac009ab1fcac540
Giovanni Perez <op7puro@live.com>
2021-01-22 09:48:03 -0500

    Add task 08

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/8-linkedlist_find/8-linkedlist_find.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class LList
+{
+    public static int FindNode(System.Collections.Generic.LinkedList<int> myLList, int value)
+    {
+        int valueIndex = 0;
+
+        foreach (int item in myLList)
+        {
+            if (item == value)
+            {
+                return valueIndex;
+            }
+            valueIndex++;
+        }
+        return -1;
+    }
+}
+

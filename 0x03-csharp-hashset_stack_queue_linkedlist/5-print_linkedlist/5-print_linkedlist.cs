04d4c237d24be073287b3dc195e151211aafc4dc
Giovanni Perez <op7puro@live.com>
2021-01-22 09:19:11 -0500

    Add task number 5

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class LList
+{
+    public static System.Collections.Generic.LinkedList<int> CreatePrint(int size)
+    {
+        
+        System.Collections.Generic.LinkedList<int> linkedNumbers = new System.Collections.Generic.LinkedList<int>();
+
+        if (size < 0)
+        {
+            return linkedNumbers;
+        }
+
+        for (int i = 0; i < size; i++)
+        {
+            linkedNumbers.AddLast(i);
+            Console.WriteLine(i);
+        }
+        return linkedNumbers;
+    }
+}

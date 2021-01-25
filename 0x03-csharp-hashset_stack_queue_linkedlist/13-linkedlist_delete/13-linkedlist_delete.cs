ba87043ceb35e3b775df1a7ac9abc61a2634bfdd
Giovanni Perez <op7puro@live.com>
2021-01-24 20:38:34 -0500

    Add task 13

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class LList
+{
+    public static void Delete(System.Collections.Generic.LinkedList<int> myLList, int index)
+    {
+        System.Collections.Generic.LinkedListNode<int> node = myLList.First;
+        int positionList = 0;
+        while (node != null)
+        {
+            if (positionList == index)
+            {
+                myLList.Remove(node);
+            }
+            positionList++;
+            node = node.Next;
+        }
+    }
+}
+

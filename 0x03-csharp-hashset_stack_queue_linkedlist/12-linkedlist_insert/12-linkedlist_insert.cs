6ae8b0f99f69b4f2fbca4afeff3aa2c6e03712f7
Giovanni Perez <op7puro@live.com>
2021-01-24 20:12:08 -0500

    Add task 12

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class LList
+{
+    public static System.Collections.Generic.LinkedListNode<int> Insert(System.Collections.Generic.LinkedList<int> myLList, int n)
+    {
+        System.Collections.Generic.LinkedListNode<int> firstNode = myLList.First;
+        System.Collections.Generic.LinkedListNode<int> newNode;
+
+        System.Collections.Generic.LinkedListNode<int> previousNode = null;
+        while (firstNode != null)
+        {
+            if (firstNode.Previous != null)
+            {
+                previousNode = firstNode.Previous;
+            }
+            
+            if (previousNode != null && previousNode.Value < n && firstNode.Value > n)
+            {
+                newNode = myLList.AddAfter(previousNode, n);
+                
+            }
+            firstNode = firstNode.Next;
+        }
+        return firstNode;
+
+
+    }
+}
+

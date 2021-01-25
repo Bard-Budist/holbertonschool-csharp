8b65be048b3ad065990463ec6ea5c7d69a9bb914
Giovanni Perez <op7puro@live.com>
2021-01-24 19:42:39 -0500

    Add task 09

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/9-linkedlist_pop/9-linkedlist_pop.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class LList
+{
+    public static int Pop(System.Collections.Generic.LinkedList<int> myLList)
+    {
+        System.Collections.Generic.LinkedListNode<int> elementeDeleted;
+        if (myLList.Count == 0)
+        {
+            return 0;
+        } else 
+        {
+            elementeDeleted = myLList.First;
+            myLList.RemoveFirst();
+            return elementeDeleted.Value;
+        }
+
+    }
+}
+

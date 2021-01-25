395f7b562499b408f3e3298c72cfb987b8468c78
Giovanni Perez <op7puro@live.com>
2021-01-24 19:56:29 -0500

    Add task 11

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/11-linkedlist_sum/11-linkedlist_sum.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class LList
+{
+    public static int Sum(System.Collections.Generic.LinkedList<int> myLList)
+    {
+        int sumElements = 0;
+
+        foreach (int item in myLList)
+        {
+            sumElements += item;
+        }
+        return sumElements;
+    }
+}
+

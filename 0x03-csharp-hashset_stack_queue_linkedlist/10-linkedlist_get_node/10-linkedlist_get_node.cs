bf80ee9b0e6aefec02ea45061d137b1a5de534c3
Giovanni Perez <op7puro@live.com>
2021-01-24 19:50:39 -0500

    Add task 10

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class LList
+{
+    public static int GetNode(System.Collections.Generic.LinkedList<int> myLList, int n)
+    {
+        int numberIndex = 0;
+        foreach (int item in myLList)
+        {
+            if (numberIndex == n)
+            {
+                return item;
+            }
+            numberIndex++;
+        }
+        return 0;
+    }
+}
+

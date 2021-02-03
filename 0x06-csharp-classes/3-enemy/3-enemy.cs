09fdb20f6d6ba517dbc4b5780065d5d05829d430
Giovanni Perez <op7puro@live.com>
2021-02-02 20:43:12 -0500

    Add task 03

---
+++ 0x06-csharp-classes/3-enemy/3-enemy.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Enemies
+{
+    class Zombie
+    {
+        private int health;
+
+        public Zombie()
+        {
+            health = 0;
+        }
+
+        public Zombie(int value)
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            } else
+            {
+                health = value;
+            }
+        }
+
+        public int GetHealth()
+        {
+            return health;
+        }
+        
+    }
+}

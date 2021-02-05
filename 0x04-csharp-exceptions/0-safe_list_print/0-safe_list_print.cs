a89c3c1b9499b23d81b3982d19053c790205272d
Giovanni Perez <op7puro@live.com>
2021-02-05 09:44:01 -0500

    Add task 00

---
+++ 0x04-csharp-exceptions/0-safe_list_print/0-safe_list_print.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class List
+{
+    public static int SafePrint(System.Collections.Generic.List<int> myList, int n)
+    {
+        int count = myList.Count, i;
+        for (i = 0; i < count; i++)
+        {
+            Console.WriteLine("{0}", myList[i]);
+            if (i + 1 == n)
+            {
+                return i + 1;
+            }
+        }
+        return count;
+    }
+}
+

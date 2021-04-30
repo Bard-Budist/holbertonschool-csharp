0b6dad946ab9531017623b01eb119c9bf25d5358
Giovanni Perez <op7puro@live.com>
2021-04-29 22:07:39 -0500

    Add task 03

---
+++ 0x07-csharp-tdd/3-palindrome/Text.Tests/Text.Tests.cs
@@ -0,0 +1,28 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Tests
+{
+    public class PalimdromeTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            string test = "A mi loca Colima";
+            Assert.That(Text.Str.IsPalindrome(test), Is.True);
+        }
+
+        [Test]
+        public void Test2()
+        {
+            string test = "A mi loca Colimaaaa";
+            Assert.That(Text.Str.IsPalindrome(test), Is.False);
+        }
+    }
+}

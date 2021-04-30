5e27c19aad3ac0682c716e116bc8c3d042524bc1
Giovanni Perez <op7puro@live.com>
2021-04-29 19:18:32 -0500

    Add task 02

---
+++ 0x07-csharp-tdd/2-max_int/MyMath.Tests/MyMath.Tests.cs
@@ -0,0 +1,24 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace MyMath.Tests
+{
+    public class MaxTest
+    {
+        [Test]
+        public void Test1()
+        {
+            List<int> numbers = new List<int>() {1, 2, 3, 4, 5, 6, 7, 9};
+            int max = Operations.Max(numbers);
+            Assert.AreEqual(max, 9);
+        }
+
+        [Test]
+        public void Test2()
+        {
+            List<int> numbers = new List<int>() {};
+            int max = Operations.Max(numbers);
+            Assert.AreEqual(max, 0);
+        }
+    }
+}
---
+++ 0x07-csharp-tdd/2-max_int/MyMath/MyMath.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MyMath
+{
+    public class Operations
+    {
+        public static int Max(System.Collections.Generic.List<int> nums)
+        {
+            if (nums.Count == 0)
+            {
+                return 0;
+            }
+
+            int max = 0;
+            foreach (int num in nums)
+            {
+                if (num > max)
+                {
+                    max = num;
+                }
+            }
+            return max;
+        }
+    }
+}

688f580184fba1c67c9e7280dbf56e1398fed87f
Giovanni Perez <op7puro@live.com>
2021-04-29 19:38:32 -0500

    update

--- 0x07-csharp-tdd/2-max_int/MyMath.Tests/MyMath.Tests.cs
+++ 0x07-csharp-tdd/2-max_int/MyMath.Tests/MyMath.Tests.cs
@@ -8,7 +8,7 @@ namespace MyMath.Tests
         [Test]
         public void Test1()
         {
-            List<int> numbers = new List<int>() {1, 2, 3, 4, 5, 6, 7, 9};
+            List<int> numbers = new List<int>() {1, 2, 3, 4, 5, 6, 7, 9, -727};
             int max = Operations.Max(numbers);
             Assert.That(max, Is.EqualTo(9));
         }

dc09eec551d70adc9c7fe1936ed7fb0eb761d946
Giovanni Perez <op7puro@live.com>
2021-05-19 15:07:15 -0500

    Add task 01

---
+++ 0x08-csharp-inheritance/1-instance/1-instance.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Obj
+{
+    public static bool IsInstanceOfArray(object obj)
+    {
+        if (obj.GetType() == typeof(int[]))
+        {
+            return true;
+        }
+        return false;
+    }
+}
+

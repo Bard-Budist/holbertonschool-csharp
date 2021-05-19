c82b7f23d746b0aeae8ce6c8bb38c155e2e1cf6a
Giovanni Perez <op7puro@live.com>
2021-05-19 15:11:18 -0500

    Fix task

--- 0x08-csharp-inheritance/1-instance/1-instance.cs
+++ 0x08-csharp-inheritance/1-instance/1-instance.cs
@@ -4,7 +4,7 @@ class Obj
 {
     public static bool IsInstanceOfArray(object obj)
     {
-        if (obj.GetType() == typeof(int[]))
+        if (obj.GetType() == typeof(int[]) || obj.GetType() == typeof(int[,]))
         {
             return true;
         }
--- 0x08-csharp-inheritance/1-instance/1-main.cs
+++ 0x08-csharp-inheritance/1-instance/1-main.cs
@@ -6,10 +6,11 @@ class Program
     static void Main(string[] args)
     {
         var numArray = new int[10];
+        int[,] a = {{ 1, 2, 3 }, { 4, 5, 6 }};
         var num = 10;
         var aList = new List<int>();
 
-        if (Obj.IsInstanceOfArray(numArray))
+        if (Obj.IsInstanceOfArray(a))
             Console.WriteLine("{0} is an instance of type array or inherits from type array", nameof(numArray));
         else
             Console.WriteLine("{0} is NOT an instance of type array nor inherits from type array", nameof(numArray));

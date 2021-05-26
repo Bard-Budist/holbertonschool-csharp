0c7714af36e9b59c6b6fa853e27ed4c6aeb5abe4
Giovanni Perez <op7puro@live.com>
2021-05-26 15:49:27 -0500

    Fix tasl

--- 0x09-csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
+++ 0x09-csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
@@ -1,9 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 
-public static class Extensions
+class VectorMath
 {
-    public static T[] Append<T>(this T[] array, T item)
+    public static double[] Add(double[] vector1, double[] vector2)
+    {
+        double[] result = new double[0];
+        if ((vector1.Length < 2 || vector1.Length > 3) || (vector2.Length < 2 || vector2.Length > 3) || (vector1.Length != vector2.Length))
+        {
+            result = Append(result, -1);
+            return result;
+        }
+
+        for (int i = 0; i < vector1.Length; i++)
+        {
+            double sum = vector1[i] + vector2[i];
+            result = Append(result, sum);
+        }
+
+        return result;
+    }
+
+    public static T[] Append<T>(T[] array, T item)
     {
         if (array == null) {
             return new T[] { item };
@@ -13,25 +31,5 @@ public static class Extensions
         result[array.Length] = item;
         return result;
     }
-}
-class VectorMath
-{
-    public static double[] Add(double[] vector1, double[] vector2)
-    {
-        double[] result = new double[0];
-        if ((vector1.Length < 2 || vector1.Length > 3) || (vector2.Length < 2 || vector2.Length > 3) || (vector1.Length != vector2.Length))
-        {
-            result.Append(-1);
-            return result;
-        }
-
-        for (int i = 0; i < vector1.Length; i++)
-        {
-            double sum = vector1[i] + vector2[i];
-            result = result.Append(sum);
-        }
-
-        return result;
-    } 
 }
 

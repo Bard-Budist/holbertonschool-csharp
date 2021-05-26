32e8544f4881b99be249c8954796f1f4e878488b
Giovanni Perez <op7puro@live.com>
2021-05-26 12:12:04 -0500

    Add task 03

---
+++ 0x09-csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class VectorMath
+{
+    public static double Magnitude(double[] vector)
+    {
+        if (vector.Length < 2)
+        {
+            return -1;
+        }
+        double total = 0;
+        foreach (double value in vector)
+        {
+            total = total + Math.Pow(value, 2);
+        }
+
+        return Math.Round(Math.Sqrt(total));
+    }
+}
+
